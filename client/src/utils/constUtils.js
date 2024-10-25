cb4ab6486451c25c31ce98c83e7c1617ba6a229b
joymahata22 <113142254+joymahata22@users.noreply.github.com>
2024-10-25 13:00:29 +0530

    ok

--- client/src/utils/constUtils.js
+++ client/src/utils/constUtils.js
@@ -1,7 +1,7 @@
-// export const proxy = "https://careerprep-production.up.railway.app/api/";
+export const proxy = "https://careerprep-5pju.onrender.com/api/";
 
-// export const AUTH_API_END_POINT = "https://careerprep-production.up.railway.app/api/auth/";
+export const AUTH_API_END_POINT = "https://careerprep-5pju.onrender.com/api/auth/";
 
 
-export const proxy = "http://localhost:5000/api/";
-export const AUTH_API_END_POINT = "http://localhost:5000/api/auth/";
+// export const proxy = "http://localhost:5000/api/";
+// export const AUTH_API_END_POINT = "http://localhost:5000/api/auth/";
